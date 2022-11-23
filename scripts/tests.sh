300a11381e0738c6f36c7bc3cc4a6c2e0babb8df
stepan <jomolun@yandex.com>
2022-11-23 23:27:21 +0300

    coverage fail fixed

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,5 +4,5 @@ path=$(pwd)
 # server
 cd ./server/build && ctest -V -R
 
-lcov -t "tests/" -o coverage.info -c -d net/ logic/
+lcov -t "tests/" -o coverage.info -c -d net/
 genhtml -o report coverage.info
